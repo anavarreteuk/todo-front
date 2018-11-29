72a05e9cb4e3406de265fa4dfdd02027c36083d4
anavarreteuk <a.navarrete.uk@gmail.com>
2018-11-29 22:04:35 +0000

    button

--- src/components/LeftDiv.js
+++ src/components/LeftDiv.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Grid, Segment} from 'semantic-ui-react'
+import { Grid, Segment, Button, Form} from 'semantic-ui-react'
 import UserList from './UserList'
 
 const leftDiv = (props) => (
 <Grid.Column padded width={4}>
         <Segment basic>
+            <Form ><Form.Field width={7}>
+                <label>New User:</label>
+                <input />
+            </Form.Field><Button type='submit'>Submit</Button>
+            </Form>
+            <br>
+            </br>
+            <br>
+            </br>
             <UserList users={props.users} selectUserHandler={props.selectUserHandler}/>
         </Segment>
 </Grid.Column> )
