3aaf9120000d0b68fd019cfddc24e8841ac4048b
github username <github email address>
2018-11-30 17:07:49 +0000

    left div changes

--- src/components/LeftDiv.js
+++ src/components/LeftDiv.js
@@ -1,21 +1,19 @@
 
 import React, { Component } from 'react';
-import { Grid, Segment, Button, Form } from 'semantic-ui-react'
+import { Grid, Segment, Button, Form, Table, Input } from 'semantic-ui-react'
 import UserList from './UserList'
-import NewUserInputBox from './NewUserInputBox';
-
 
 
 const leftDiv = (props) => (
     <Grid.Column width={4}>
-             <Segment basic>
-                <Form onSubmit={props.handleSubmit}><Form.Field width={7}>
-                        <label>New User:</label>
-                        <input  onChange={props.handleChange} />
-                    </Form.Field><Button type='submit' > Submit</Button>
-                    </Form>
-            </Segment>
             <Segment basic>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>
+                            <Input/>
+                        </Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
                 <UserList users={props.users} selectUserHandler={props.selectUserHandler}/>
             </Segment>
     </Grid.Column> )
